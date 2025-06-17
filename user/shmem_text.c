0a9e5fc4ce613e8790a56bb649fd823929f84434
TomD49 <tomerdvir4.9@gmail.com>
2025-06-17 15:05:15 +0000

    another

--- user/shmem_text.c
+++ user/shmem_text.c
@@ -8,23 +8,23 @@ main(int argc, char *argv[])
     printf("%s\n", shmem_text);
     int parent_pid = getpid();
     if(fork() == 0) {
-        printf("1. Child process size before shared mapping: %d\n", );
+        printf("1. Child process size before shared mapping: %d\n", sbrk(0));
         int va= map_shared_pages(parent_pid, getpid(), (int)shmem_text, sizeof(shmem_text));
-        printf("2. Child process size after shared mapping: %d\n",);
+        printf("2. Child process size after shared mapping: %d\n",sbrk(0));
         strcpy(va, "Hello daddy");
         if(unmap_shared_pages(getpid(), va, sizeof(shmem_text))<0){
             printf("Error unmapping shared memory\n");
             exit(1);
         }
-        printf("3. Child process size after delete shared mapping: %d\n",);
+        printf("3. Child process size after delete shared mapping: %d\n",sbrk(0));
         malloc(100);
-        printf("4. Child process size after malloc: %d\n",);
+        printf("4. Child process size after malloc: %d\n",sbrk(0));
         
     }
     else{
         sleep(5);
         printf("Parent process text : %s\n", shmem_text);
-        printf("Parent process size: %d\n", );}
+        printf("Parent process size: %d\n", sbrk(0));}
     
     // Exit the program
     exit(0);
